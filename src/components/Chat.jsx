c9787f07241c7bcb7fdcf0bbfca71fc40e7ab83f
hongvy0707 <hongvy0707@gmail.com>
2023-02-23 16:39:13 +0700

    merge DarkMode and button More

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,69 +1,73 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import Messages from './Messages';
 import Input from './Input';
-import Camera from './Camera';
-import Modal from 'react-modal';
 import { ChatContext } from "../context/ChatContext";
 import DarkMode from './DarkMode';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Chat = () => {
-  const [showCameraModal, setShowCameraModal] = useState(false);
-  const [showCamera, setShowCamera] = useState(false); // Thêm state để kiểm soát việc hiển thị camera
   const { data } = useContext(ChatContext);
-  const openCameraModal = () => {
-    setShowCameraModal(true);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const [showDarkModeText, setShowDarkModeText] = useState(true);
+  const [darkModeEnabled, setDarkModeEnabled] = useState(false);
+
+  const darkModeHandler = () => {
+    <DarkMode></DarkMode>
+    setDarkModeEnabled(!darkModeEnabled);
   };
 
-  const closeCameraModal = () => {
-    setShowCameraModal(false);
-  };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
 
-  const openCamera = () => {
-    setShowCamera(true);
-    closeCameraModal();
-  };
-
-  const closeCamera = () => {
-    setShowCamera(false);
-  };
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [dropdownRef]);
 
   return (
     <div className="chat">
       <div className="chatInfo">
         <span>{data.user?.displayName}</span>
         <div className="chatIcons">
-        <DarkMode/>
+          <DarkMode></DarkMode>
           <img
             src="https://res.cloudinary.com/dijk3xi4c/image/upload/v1676465117/cam_f1a3q2.png"
             alt="Camera"
-            onClick={openCameraModal}
           />
           <img
             src="https://res.cloudinary.com/dijk3xi4c/image/upload/v1676465117/add_oarqxw.png"
             alt="Add"
           />
-          <img
-            src="https://res.cloudinary.com/dijk3xi4c/image/upload/v1676465117/more_jpxt6w.png"
-            alt="More"
-          />
+          <Navbar>
+            <NavDropdown title={<img src='https://res.cloudinary.com/dijk3xi4c/image/upload/v1676465117/more_jpxt6w.png' 
+                                alt='More'/>}
+                                onClick={() => {
+                                  setDropdownOpen(!dropdownOpen);
+                                  setShowDarkModeText(true);
+                                }}
+                                ref={dropdownRef}
+                                onToggle={() => setShowDarkModeText(false)}>
+              <NavDropdown.Item>Action</NavDropdown.Item>
+              <NavDropdown.Item>Another Action</NavDropdown.Item>
+              <NavDropdown.Divider />
+                {showDarkModeText && (
+                  <NavDropdown.Item>
+                    <DarkMode onClick={darkModeHandler}/>
+                  </NavDropdown.Item>
+                )}
+            </NavDropdown>
+          </Navbar>
         </div>
       </div>
-      {showCamera && (
-        <div className="cameraOverlay">
-          <Camera closeCamera={closeCamera} />
-        </div>
-      )}
-      <Messages showCamera={showCamera} />
-      <Input />
-      <Modal
-        isOpen={showCameraModal}
-        onRequestClose={closeCameraModal}
-        contentLabel="Camera Modal"
-      >
-        <Camera closeCamera={closeCamera} />
-        <button onClick={openCamera}>Open Camera</button>
-        <button onClick={closeCameraModal}>Close</button>
-      </Modal>
+      <Messages darkModeEnabled={darkModeEnabled} />
+      <Input darkModeEnabled={darkModeEnabled} />
     </div>
   );
 };
